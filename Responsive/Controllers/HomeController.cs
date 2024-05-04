f2a36524dc88391614a95dfd52309a0208fefd91
mahesh <maheshkamalay777@gmail.com>
2024-05-04 19:51:30 +0530

    initial commit

---
+++ Responsive/Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using Antlr.Runtime.Misc;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using static System.Web.Razor.Parser.SyntaxConstants;
+using System.Web.Services.Description;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using System.Xml.Linq;
+
+namespace Responsive.Controllers
+{
+    //[Authorize(Roles ="Domain//Windows")]
+    public class HomeController : Controller
+    {
+       
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-EH7863I\\SQLEXPRESS; Initial Catalog=Login; Integrated Security=True");
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult OurServices()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Demo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+
+        }
+        public ActionResult ContactUs()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Issues()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+            public ActionResult Add_data(string name, string email, string password, int age, string gender, string city, string country, string pincode)
+            {
+            string msg = string.Empty;
+                try
+                {
+                
+                    SqlCommand command = new SqlCommand("InsertRegistrationData", con);
+                            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                            command.Parameters.AddWithValue("@Name", name);
+                            command.Parameters.AddWithValue("@Email", email);
+                            command.Parameters.AddWithValue("@Password", password);
+                            command.Parameters.AddWithValue("@Age", age);
+                            command.Parameters.AddWithValue("@Gender", gender);
+                            command.Parameters.AddWithValue("@City", city);
+                            command.Parameters.AddWithValue("@Country", country);
+                            command.Parameters.AddWithValue("@Pincode", pincode);
+                        con.Open();
+                        command.ExecuteNonQuery();
+                        con.Close();
+                        msg = "Data Inserted";
+                
+                return Json(msg, JsonRequestBehavior.AllowGet);
+                        }
+                        catch (Exception ex)
+            {
+                msg = "Error: " + ex.Message;
+            }
+            return Json(msg, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+
+                   
+ }
---
+++ Responsive/JavaScript.js
@@ -0,0 +1,42 @@
+﻿ $(document).ready(function() {
+        $('#btnSubmit').click(function (event) {
+            // Prevent form submission
+            event.preventDefault();
+
+            // Perform form validation
+            var name = $('#name').val();
+            var email = $('#email').val();
+            var password = $('#password').val();
+            var age = $('#age').val();
+            var gender = $('#gender').val();
+            var city = $('#city').val();
+            var country = $('#country').val();
+            var pincode = $('#pincode').val();
+
+            var emailRegex = /^[^\s@]+@gmail\.com$/;
+            var passwordRegex = /^(?=.*\d).{8,}$/;
+
+            if (name === "") {
+                alert("Please Enter Name");
+            } else if (!emailRegex.test(email)) {
+                alert("Please enter a valid email address.");
+            } else if (!passwordRegex.test(password)) {
+                alert("Password must be at least 8 characters long and contain at least one number.");
+            } else if (age === "") {
+                alert("Please Enter Age");
+            } else if (city === "") {
+                alert("Please Enter City");
+            } else if (country === "") {
+                alert("Please Enter Country");
+            } else if (pincode === "") {
+                alert("Please Enter Pincode");
+            } else {
+                // If all validations pass, submit the form
+                alert(' Thanks For Register...! We Will Get Back To you');
+                // Uncomment the line below to submit the form
+                // $(this).unbind('submit').submit();
+            }
+        });
+    });
+
+
